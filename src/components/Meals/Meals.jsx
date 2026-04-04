4d324e2bc811c5187ae15a720aaf0cd61159b124
Nurik-04 <alpisbayevnurdaulet@gmail.com>
2026-04-04 11:21:58 +0500

    Meals.css bolimi qoshildi

--- src/components/Meals/Meals.jsx
+++ src/components/Meals/Meals.jsx
@@ -4,6 +4,7 @@ import './meals.css';
 
 // 1. Har bir ovqat kartochkasi uchun alohida komponent
 const MealCard = ({ el }) => {
+
     const [count, setCount] = useState(1); // Har bir ovqat uchun alohida state
 
     const inc = () => setCount(count + 1);
@@ -12,14 +13,15 @@ const MealCard = ({ el }) => {
     };
 
     return (
-        <div className="meal__box">
+        <div className="meal__card">
+            
             <img src={el.image} alt={el.name} className="meal__img" />
             <h3 className="meal__name">{el.name}</h3>
             <p className="meal__description">{el.description}</p>
-            
+
             {/* Narxni miqdorga ko'paytirib ko'rsatish */}
             <span className="meal__price">R$ {(el.price * count).toFixed(2)}</span>
-            
+
             <div className="meal__footer">
                 <div className='meal__caunter'>
                     <button onClick={dec} className='meal__caunter-btn'>-</button>
@@ -33,13 +35,17 @@ const MealCard = ({ el }) => {
 };
 
 const Meals = () => {
+
     return (
         <div>
             <div className='container'>
                 <div className="meals__container">
-                    {ovqatlar.map((el) => (
-                        <MealCard key={el.id} el={el} />
-                    ))}
+                    <h2 className='meals__title'>Refeições</h2>
+                    <div className="mealt__list">
+                        {ovqatlar.map((el) => (
+                            <MealCard key={el.id} el={el} />
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
